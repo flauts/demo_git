b0fe30b006395bbf097430ff2718626a5b46d66a
flauts <hector.nieto@utec.edu.pe>
2024-04-12 14:21:56 -0500

    orv

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print("Este es mi primer proyecto con git xd")
+print("Commit sin usar add xd")
+KDJsCompany = ["KDJ","YJK","HSY","LGY","YSG","LHS","JHW","SYS","LJH"]
+
+for member in KDJsCompany:
+    print(51)
+    print(49)
+
 print("Hello World")
 
